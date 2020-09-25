c4b93760d703d055f80b71a36b561c0736a9aecf
Ranbir7925 <honeyranbir7925@gmail.com>
2020-09-25 13:20:59 +0530

    [Ranbir]ADDED:Test case for checking wrong file type

--- src/test/scala/CensusAnalyserTest.scala
+++ src/test/scala/CensusAnalyserTest.scala
@@ -1,9 +1,16 @@
+import com.bridgelabz.indiancensusanalser.{CensusAnalyser, CensusAnalyzerExceptionEnums}
 import org.scalatest.FunSuite
 
-class CensusAnalyserTest extends FunSuite{
-  test("given_IndianCensusCSVFile_ShouldReturnsCorrectRecords"){
+class CensusAnalyserTest extends FunSuite {
+  test("given_IndianCensusCSVFile_ShouldReturnsCorrectRecords") {
     val objCensus = new CensusAnalyser()
-    assert(objCensus.loadIndiaCensusData("./src/test/resources/IndiaStateCensusData.csv")==29)
+    assert(objCensus.loadIndiaCensusData("./src/test/resources/IndiaStateCensusData.csv") == 29)
   }
-
+  test("givenIndianCensusDataCSVFile_whenWithWrongFilePath_ShouldThrowException") {
+    val throws = intercept[Exception] {
+      val objCensus = new CensusAnalyser()
+      objCensus.loadIndiaCensusData("./src/resources/IndiaStateCensusData.csv")
+    }
+    assert(throws.getMessage == CensusAnalyzerExceptionEnums.inCorrectFilePath.toString)
+  }
 }
