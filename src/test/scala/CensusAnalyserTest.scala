743aa759ba5b6f3e015f334f01266ca8ec99c47b
Ranbir7925 <honeyranbir7925@gmail.com>
2020-09-25 20:15:43 +0530

    [Ranbir]ADDED:Test case for checking wrong csv file type

--- src/test/scala/CensusAnalyserTest.scala
+++ src/test/scala/CensusAnalyserTest.scala
@@ -2,15 +2,25 @@ import com.bridgelabz.indiancensusanalser.{CensusAnalyser, CensusAnalyzerExcepti
 import org.scalatest.FunSuite
 
 class CensusAnalyserTest extends FunSuite {
-  test("given_IndianCensusCSVFile_ShouldReturnsCorrectRecords") {
+  val INDIA_STATE_CODE_CSV_FILE_PATH  = "./src/test/resources/IndiaStateCensusData.csv"
+  val STATE_CODE_CSV_WRONG_FILE_PATH = "./src/resources/IndiaStateCensusData.csv"
+  val STATE_CODE_CSV_WRONG_TYPE_PATH = "./src/test/resources/IndiaStateCensusData.txt"
+  test("given_IndianCensusCSVFile_ShouldReturnCorrectRecords") {
     val objCensus = new CensusAnalyser()
-    assert(objCensus.loadIndiaCensusData("./src/test/resources/IndiaStateCensusData.csv") == 29)
+    assert(objCensus.loadIndiaCensusData(INDIA_STATE_CODE_CSV_FILE_PATH) == 29)
   }
   test("givenIndianCensusDataCSVFile_whenWithWrongFilePath_ShouldThrowException") {
     val throws = intercept[Exception] {
       val objCensus = new CensusAnalyser()
-      objCensus.loadIndiaCensusData("./src/resources/IndiaStateCensusData.csv")
+      objCensus.loadIndiaCensusData(STATE_CODE_CSV_WRONG_FILE_PATH)
     }
     assert(throws.getMessage == CensusAnalyzerExceptionEnums.inCorrectFilePath.toString)
+  }
+  test("givenIndianCensusDataCSVFile_whenWithWrongFileType_ShouldThrowException"){
+    val throws= intercept[Exception]{
+      val objCensus = new CensusAnalyser
+      objCensus.loadIndiaCensusData(STATE_CODE_CSV_WRONG_TYPE_PATH)
+    }
+    assert(throws.getMessage == CensusAnalyzerExceptionEnums.inCorrectFileType.toString)
   }
 }
