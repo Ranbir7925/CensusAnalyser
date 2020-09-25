28e121ca9c79aaa70c7703a0f996e9b545611ddc
Ranbir7925 <honeyranbir7925@gmail.com>
2020-09-25 20:05:06 +0530

    [Ranbir]REFACTORED:Code to check invalid file type

--- src/main/scala/com/bridgelabz/indiancensusanalser/CensusAnalyser.scala
+++ src/main/scala/com/bridgelabz/indiancensusanalser/CensusAnalyser.scala
@@ -7,6 +7,9 @@ import scala.io.Source
 class CensusAnalyser {
   def loadIndiaCensusData(filePath: String): Int = {
     try {
+      if(!filePath.endsWith(".csv")){
+        throw new CensusAnalyserException(CensusAnalyzerExceptionEnums.inCorrectFileType)
+      }
       val fileReader = Source.fromFile(filePath)
       var countRow = 0
       for (_ <- fileReader.getLines()) {
@@ -16,8 +19,6 @@ class CensusAnalyser {
     }
     catch {
       case ex: FileNotFoundException => throw new CensusAnalyserException(CensusAnalyzerExceptionEnums.inCorrectFilePath)
-        1
-
     }
   }
 }
