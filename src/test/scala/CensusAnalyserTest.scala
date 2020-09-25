31318454bdaff4d2ab53b3833d3882decf8241dc
Ranbir7925 <honeyranbir7925@gmail.com>
2020-09-25 20:54:12 +0530

    [Ranbir]ADDED:Test case to check invalid delimiters

--- src/test/scala/CensusAnalyserTest.scala
+++ src/test/scala/CensusAnalyserTest.scala
@@ -2,25 +2,36 @@ import com.bridgelabz.indiancensusanalser.{CensusAnalyser, CensusAnalyzerExcepti
 import org.scalatest.FunSuite
 
 class CensusAnalyserTest extends FunSuite {
-  val INDIA_STATE_CODE_CSV_FILE_PATH  = "./src/test/resources/IndiaStateCensusData.csv"
-  val STATE_CODE_CSV_WRONG_FILE_PATH = "./src/resources/IndiaStateCensusData.csv"
-  val STATE_CODE_CSV_WRONG_TYPE_PATH = "./src/test/resources/IndiaStateCensusData.txt"
+  val INDIA_CENSUS_CSV_FILE_PATH   = "./src/test/resources/IndiaStateCensusData.csv"
+  val WRONG_CSV_FILE_PATH  = "./src/resources/IndiaStateCensusData.csv"
+  val WRONG_CSV_FILE_TYPE_PATH  = "./src/test/resources/IndiaStateCensusData.txt"
+  val INVALID_DELIMITER_FILE_PATH = "./src/test/resources/InvalidDelimitersIndiaStateCensusData.csv"
+
   test("given_IndianCensusCSVFile_ShouldReturnCorrectRecords") {
     val objCensus = new CensusAnalyser()
-    assert(objCensus.loadIndiaCensusData(INDIA_STATE_CODE_CSV_FILE_PATH) == 29)
+    assert(objCensus.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH) == 29)
   }
   test("givenIndianCensusDataCSVFile_whenWithWrongFilePath_ShouldThrowException") {
     val throws = intercept[Exception] {
       val objCensus = new CensusAnalyser()
-      objCensus.loadIndiaCensusData(STATE_CODE_CSV_WRONG_FILE_PATH)
+      objCensus.loadIndiaCensusData(WRONG_CSV_FILE_PATH)
     }
     assert(throws.getMessage == CensusAnalyzerExceptionEnums.inCorrectFilePath.toString)
   }
+
   test("givenIndianCensusDataCSVFile_whenWithWrongFileType_ShouldThrowException"){
     val throws= intercept[Exception]{
       val objCensus = new CensusAnalyser
-      objCensus.loadIndiaCensusData(STATE_CODE_CSV_WRONG_TYPE_PATH)
+      objCensus.loadIndiaCensusData(WRONG_CSV_FILE_TYPE_PATH)
     }
     assert(throws.getMessage == CensusAnalyzerExceptionEnums.inCorrectFileType.toString)
+  }
+
+  test("givenIndianCensusDataCSVFile_whenWithWrongDelimiters_shouldThrowException"){
+    val throws = intercept[Exception] {
+      val objCensus = new CensusAnalyser
+      objCensus.loadIndiaCensusData(INVALID_DELIMITER_FILE_PATH)
+    }
+    assert(throws.getMessage == CensusAnalyzerExceptionEnums.inCorrectDelimiter.toString)
   }
 }
