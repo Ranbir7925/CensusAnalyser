77d15c562cb0126461286cd6f1b8f58a40da2877
Ranbir7925 <honeyranbir7925@gmail.com>
2020-09-26 20:39:09 +0530

    [Ranbir]ADDED:Class to bind state code csv file

---
+++ src/main/scala/com/bridgelabz/indiancensusanalser/IndiaStateCodeCSV.scala
@@ -0,0 +1,15 @@
+package com.bridgelabz.indiancensusanalser
+
+case class IndiaStateCodeCSV() {
+
+  import com.opencsv.bean.CsvBindByName
+
+  @CsvBindByName(column = "SrNo", required = true) var SrNo: String = null
+  @CsvBindByName(column = "State Name", required = true) var state: String = null
+
+  @CsvBindByName(column = "TIN", required = true) var TIN: String = null
+
+  @CsvBindByName(column = "StateCode", required = true) var stateCode: String = null
+
+  override def toString: String = "IndiaStateCodeCSV{" + "SrNo='" + SrNo + '\'' + ", state='" + state + '\'' + ", TIN='" + TIN + '\'' + ", stateCode='" + stateCode + '\'' + '}'
+}
