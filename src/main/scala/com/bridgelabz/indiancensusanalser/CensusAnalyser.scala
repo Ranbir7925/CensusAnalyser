33aa23f30f9581564d4674cce38083a07a682594
Ranbir7925 <honeyranbir7925@gmail.com>
2020-09-26 12:40:15 +0530

    [Ranbir]REFACTORED:Code to checking invalid file type/headers/delimiters and also state count

--- src/main/scala/com/bridgelabz/indiancensusanalser/CensusAnalyser.scala
+++ src/main/scala/com/bridgelabz/indiancensusanalser/CensusAnalyser.scala
@@ -32,4 +32,32 @@ class CensusAnalyser {
       case ex: FileNotFoundException => throw new CensusAnalyserException(CensusAnalyzerExceptionEnums.inCorrectFilePath)
     }
   }
+
+  def loadIndiaStateCode(filePath:String):Int={
+    try {
+      if(!filePath.endsWith(".csv")){
+        throw new CensusAnalyserException(CensusAnalyzerExceptionEnums.inCorrectFileType)
+      }
+      val fileReader = Source.fromFile(filePath)
+      var countRow = 0
+      for (line <- fileReader.getLines()) {
+        val cols = line.split(",").map(_.trim)
+
+        if (cols.length != 4){
+          throw new CensusAnalyserException(CensusAnalyzerExceptionEnums.inCorrectDelimiter)
+        }
+
+        if (countRow == 0){
+          if(cols(1) != "State Name" || cols(2) != "TIN" || cols(3) != "StateCode"){
+            throw new CensusAnalyserException(CensusAnalyzerExceptionEnums.inCorrectHeaders)
+          }
+        }
+        countRow += 1
+      }
+      countRow - 1
+    }
+    catch {
+      case ex: FileNotFoundException => throw new CensusAnalyserException(CensusAnalyzerExceptionEnums.inCorrectFilePath)
+    }
+  }
 }
